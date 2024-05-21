2ffff911d9db055a26a7b0200157a8f8bf42f6cb
erik60reis <155772652+erik60reis@users.noreply.github.com>
2024-05-21 18:52:06 -0300

    Design Updates

--- assets/leaderboardfunctions.js
+++ assets/leaderboardfunctions.js
@@ -3,9 +3,9 @@ async function fetchLeaderboard() {
         const response = await fetch(`/games/${gamename}/leaderboard`);
         const leaderboardData = await response.json();
         const leaderboardTable = document.getElementById('leaderboardTable');
-        leaderboardTable.innerHTML = '<tr><th>Rank</th><th>Name</th><th>Score</th></tr>';
+        leaderboardTable.innerHTML = '<tr class="leaderboardfirstrow"><th class="aligncenter">Rank</th><th class="aligncenter">Name</th><th class="aligncenter">Score</th></tr>';
         leaderboardData.forEach((entry, index) => {
-            const row = `<tr><td>${index + 1}</td><td class="playerName"></td><td class="playerScore"></td></tr>`;
+            const row = `<tr class="leaderboardOtherRows"><td>#${index + 1}</td><td class="playerName"></td><td class="playerScore"></td></tr>`;
             leaderboardTable.innerHTML += row;
             document.getElementsByClassName("playerName")[document.getElementsByClassName("playerName").length - 1].textContent = entry.name;
             document.getElementsByClassName("playerScore")[document.getElementsByClassName("playerScore").length - 1].textContent = entry.score;
